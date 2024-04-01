bff107d9fdb02ee78fa1974d0a7393a1cb7fb1b7
Mohamed-gp <mohamedterba6@gmail.com>
2024-04-01 11:22:45 +0100

    complete pagination component

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import FeaturesSection from "../components/features/FeaturesSection";
 import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
 import PropertiesSection from "../components/properties/PropertiesSection";
 
 const Home = () => {
+  const [firstIndex,setFirstIndex] = useState<number>(0);
+  const [lastIndex,setLastIndex] = useState<number>(0);
   return (
     <>
       <Hero />
       <FeaturesSection />
-      <PropertiesSection />
-      
+      <PropertiesSection firstIndex={firstIndex} lastIndex={lastIndex} />
     </>
   );
 };
