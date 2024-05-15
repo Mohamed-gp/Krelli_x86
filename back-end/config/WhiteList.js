fb79d80bfc7cf9da05eedcc6937cd3a69a6572c7
Mohamed-gp <mohamedterba6@gmail.com>
2024-05-16 00:03:09 +0100

    fixing netlify problem

--- back-end/config/WhiteList.js
+++ back-end/config/WhiteList.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   "http://127.0.0.1:5500",
   "http://localhost:3500",
-  "https://krelli-x86-1.onrender.com",
+  "https://krilli-x86.netlify.app/",
 ];
 export default allowedOrigins;
