78da22c213e77afcaf7b5b441c9fad042e8de03f
Mohamed-gp <mohamedterba6@gmail.com>
2024-05-15 23:55:02 +0100

    editing render url for backend deployement

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const env = "developement"
 
 // const url = "http://localhost:3000/"
-const url = "https://krelli-x86-test.onrender.com"
+const url = "https://krelli-x86-1.onrender.com/"
 
 const customAxios = axios.create({  
     baseURL: url, // Set a base URL for all requests
