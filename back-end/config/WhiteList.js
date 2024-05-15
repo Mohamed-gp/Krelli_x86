8523b6b5aa0fb4b24d9e9c3bf14025a72a82ff80
Mohamed-gp <mohamedterba6@gmail.com>
2024-05-16 00:00:36 +0100

    fix cors

--- back-end/config/WhiteList.js
+++ back-end/config/WhiteList.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'http://127.0.0.1:5500',
-    'http://localhost:3500'
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "https://krelli-x86-1.onrender.com",
 ];
 export default allowedOrigins;
