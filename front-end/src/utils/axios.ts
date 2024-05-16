45c10fc8b30ab7a631e11a3196570926644dd6d9
Mohamed-gp <mohamedterba6@gmail.com>
2024-05-16 01:16:56 +0100

    last deploy try

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -1,22 +1,14 @@
 import axios from "axios";
 
-const env = "production"
+const env = "production";
 
 // const url = "http://localhost:3000/"
-const url = "https://krelli-x86-1.onrender.com"
+const url = "https://krelli-x86-1.onrender.com";
 
-const customAxios = axios.create({  
-    baseURL: url, // Set a base URL for all requests
-    // timeout: 5000, // Set a timeout for requests in milliseconds
-    // headers: {
-    //   'Content-Type': 'application/json', // Set default headers
-    //   'Authorization': 'Bearer your-access-token' // Add authorization header if needed
-    // }
-    withCredentials : true,
-
+const customAxios = axios.create({
+  baseURL: "https://krelli-x86-1.onrender.com/",
+  credentials: "include",
+  withCredentials: true,
 });
 
-
-export default customAxios
-
-
+export default customAxios;
