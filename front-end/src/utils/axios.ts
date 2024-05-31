fffa586bfa5be91db6d2a78080a921c8c015928e
Mohamed-gp <mohamedterba6@gmail.com>
2024-05-31 15:12:21 +0100

    last push

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 // const url = "http://localhost:3000/"
 // const url = "https://krelli-x86-1.onrender.com";
 const customAxios = axios.create({
-  baseURL: "https://krelli-x86-1.onrender.com/",
-  // baseURL: "http://localhost:3000/",
+  // baseURL: "https://krelli-x86-1.onrender.com/",
+  baseURL: "http://localhost:3000/",
   withCredentials: true,
 });
 
