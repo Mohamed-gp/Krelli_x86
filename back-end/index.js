1f805d02d9b02301365a1d1a10fe4284f0e1041d
Mohamed-gp <mohamedterba6@gmail.com>
2024-06-01 17:52:35 +0100

    deploy

--- back-end/index.js
+++ back-end/index.js
@@ -30,7 +30,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({ credentials: true, origin: "https://krelli-86x.netlify.app" }));
-app.use(cors({ credentials: true, origin: "http://localhost:3500" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:3500" }));
 
 const server = createServer(app);
 
