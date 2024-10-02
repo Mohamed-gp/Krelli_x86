1f2f738ab81c297db134e1c05e7a7502fd622614
Mohamed-gp <mohamedterba6@gmail.com>
2024-10-03 00:30:36 +0100

    add github action

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const customAxios = axios.create({
   baseURL:
     import.meta.env.VITE_ENV == "development"
-      ? "http://localhost:3001/api"
-      : "https://krelli1.production-server.tech/api",
+      ? "http://localhost:3003/api"
+      : "https://yumyum1.production-server.tech/api",
   // baseURL: "http://localhost:3000/",
   withCredentials: true,
 });
