8ff912c6640958e2c91a254d2f74d84f2c50cf66
Mohamed-gp <mohamedterba6@gmail.com>
2024-10-03 13:42:55 +0100

    fix axios

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -3,9 +3,8 @@ import axios from "axios";
 const customAxios = axios.create({
   baseURL:
     import.meta.env.VITE_ENV == "development"
-      ? "http://localhost:3003/api"
-      : "https://yumyum1.production-server.tech/api",
-  // baseURL: "http://localhost:3000/",
+      ? "http://localhost:3001/api"
+      : "https://krelli1.production-server.tech/api",
   withCredentials: true,
 });
 
